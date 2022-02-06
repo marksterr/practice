22e1787d9b44f664a560ab2e8e28b6cda6bf0d43
Mark Dawes <mrkdawes8@gmail.com>
2022-02-06 17:05:50 -0500

    start official score keeper

--- ScoreKeeper/app.js
+++ ScoreKeeper/app.js
@@ -1,24 +1,52 @@
-let scoreOne = 0;
-let scoreTwo = 0;
-const score = document.querySelector('h1');
-const buttonOne = document.querySelector('#buttonone');
-const buttonTwo = document.querySelector('#buttontwo');
-const resetButton = document.querySelector('#resetbutton');
+const p1Button = document.querySelector('#p1Button');
+const p2Button = document.querySelector('#p2Button');
+const resetButton = document.querySelector('#reset');
+const p1Display = document.querySelector('#p1Display');
+const p2Display = document.querySelector('#p2Display');
+const winningScoreSelect = document.querySelector('#playto');
 
-buttonOne.addEventListener('click', function(e){
-  e.preventDefault();
-  scoreOne++;
-  score.innerText = `${scoreOne} to ${scoreTwo}`;
+let p1Score = 0;
+let p2Score = 0;
+let winningScore = 3;
+let isGameOver = false;
+
+p1Button.addEventListener('click', function() {
+  if(!isGameOver) {
+    p1Score += 1;
+    if(p1Score === winningScore) {
+      isGameOver = true;
+      p1Display.classList.add('winner');
+      p2Display.classList.add('loser');
+    }
+    p1Display.textContent = p1Score;
+  }
 });
 
-buttonTwo.addEventListener('click', function(e){
-  e.preventDefault();
-  scoreTwo++;
-  score.innerText = `${scoreOne} to ${scoreTwo}`;
+p2Button.addEventListener('click', function() {
+  if(!isGameOver) {
+    p2Score += 1;
+    if(p2Score === winningScore) {
+      isGameOver = true;
+      p1Display.classList.add('loser');
+      p2Display.classList.add('winner');
+    }
+    p2Display.textContent = p2Score;
+  }
 });
 
-resetButton.addEventListener('click', function(e){
-  scoreOne = 0;
-  scoreTwo = 0;
-  score.innerText = `${scoreOne} to ${scoreTwo}`;
-});
+winningScoreSelect.addEventListener('change', function() {
+  winningScore = parseInt(this.value);
+  reset();
+})
+
+resetButton.addEventListener('click', reset)
+
+function reset() {
+  isGameOver = false;
+  p1Score = 0;
+  p2Score = 0;
+  p1Display.textContent = 0;
+  p2Display.textContent = 0;
+  p1Display.classList.remove('winner', 'loser');
+  p2Display.classList.remove('winner', 'loser');
+}
